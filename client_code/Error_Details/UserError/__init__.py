fcc95ba6e326807568421c5bc7e2403552a3dd4e
divyeshlakhotia@gmail.com <divyeshlakhotia@gmail.com>
2025-03-22 07:09:43 +0000

    Edited settings, Error_Details.UserError and Globals

--- client_code/Error_Details/UserError/__init__.py
+++ client_code/Error_Details/UserError/__init__.py
@@ -2,6 +2,8 @@ from ._anvil_designer import UserErrorTemplate
 from .. import time_operations
 from anvil import *
 from m3._Components.Text import Text
+from anvil.js import window
+from ... import Globals
 
 class UserError(UserErrorTemplate):
     def __init__(self, **properties):
@@ -22,9 +24,12 @@ class UserError(UserErrorTemplate):
             F.add_component(Text(text=field.capitalize(), bold= True, text_color="theme:On Surface Variant"))
             F.add_component(Text(text=value, bold= False), expand= True)
             self.additional_details_panel.add_component(F)
-        self.session_link.url = f"https://anvil.works/build/apps/BVKJHWRTBSCSLR4L/sessions/{self.item['additional_info']['session']}"
-
+        
     def additional_details_toggle_click(self, **event_args):
         
         self.additional_details_panel.visible = not self.additional_details_panel.visible
         self.additional_details_toggle.icon="mi:keyboard_arrow_down" if self.additional_details_panel.visible else "mi:keyboard_arrow_right"
+
+    def session_link_click(self, **event_args):
+        app_id = Globals.get_app_id()
+        window.open(f"https://anvil.works/build/apps/{app_id}/sessions/{self.item['additional_info']['session']}", "_blank")
--- client_code/Globals.py
+++ client_code/Globals.py
@@ -1,4 +1,21 @@
+from anvil import *
+from m3.components import TextBox
+import anvil.server
+
 app_id = None
 last_opened = None
 active_error = None
 
+def get_app_id():
+    global app_id
+    if not app_id:
+        
+        Container = LinearPanel(spacing_below="none")
+        APP_ID_Input = TextBox(label = "Enter App ID")
+        Container.add_component(APP_ID_Input)
+        Container.add_component(Label(text="Your APP ID will be used for generating dynamic Anvil Editor URLs", spacing_below="none"))
+        alert(Container, title= "Set up your APP ID", dismissible= False, buttons=["Submit"])
+        app_id = APP_ID_Input.text
+        anvil.server.call("set_app_id", app_id)
+
+    return app_id
